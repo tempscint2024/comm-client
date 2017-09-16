1fa90b6b23752e023e7cec02ffdaa234d018753f
Winner musole <musolemasu575@gmail.com>
2017-09-16 20:18:07 +0300

    Addition of a shopping cart,authenticate boolean to show the avatar for authenticated user

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <v-app light>
 
     <v-navigation-drawer absolute persistent light v-model="drawer" overflow>
-      <v-toolbar flat class="transparent">
-        <v-list class="pa-0">
+      <v-toolbar flat class="transparent" v-if="authenticate">
+        <v-list class="pa-0" >
           <v-list-tile avatar>
             <v-list-tile-avatar>
               <img src="static/img/avatar-image.jpg" />
@@ -14,7 +14,7 @@
           </v-list-tile>
         </v-list>
       </v-toolbar>
-      <v-list class="pt-0" dense>
+      <v-list class="pt-0" dense v-else>
         <v-divider></v-divider>
         <v-list-tile v-for="item in items" :key="item.title" @click="">
           <v-list-tile-action>
@@ -28,7 +28,13 @@
     </v-navigation-drawer>
     <v-toolbar fixed class="indigo darken-4" dark>
       <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title class="toolbar-title">ShopIt</v-toolbar-title>
+      <v-toolbar-title class="toolbar-title hidden-sm-and-down">{{ title }}</v-toolbar-title>
+      <v-spacer></v-spacer>
+
+        <v-btn icon>
+          <v-icon v-badge="{ value: 6, left: true, }">shopping_cart</v-icon>
+        </v-btn>
+
     </v-toolbar>
     <main>
 
@@ -47,7 +53,9 @@
           { title: 'Home', icon: 'dashboard' },
           { title: 'About', icon: 'question_answer' }
         ],
-        right: null
+        right: null,
+        title: 'ShopIt',
+        authenticate: false
       }
     }
   }
